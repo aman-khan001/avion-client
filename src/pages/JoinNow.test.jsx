7d61cb5538fe7b28a01768f08262c97c8a02ab34
aman-khan001 <amanazadkhan.ak@gmail.com>
2026-07-23 15:18:04 +0530

    social enquiries form added

--- src/pages/JoinNow.test.jsx
+++ src/pages/JoinNow.test.jsx
@@ -1,15 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import { HelmetProvider } from "react-helmet-async";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import JoinNow from "./JoinNow";
 
 describe("JoinNow", () => {
   it("shows the ad source from the query string", () => {
     render(
-      <MemoryRouter initialEntries={["/join-now?source=facebook-ad"]}>
-        <Routes>
-          <Route path="/join-now" element={<JoinNow />} />
-        </Routes>
-      </MemoryRouter>,
+      <HelmetProvider>
+        <MemoryRouter initialEntries={["/join-now?source=facebook-ad"]}>
+          <Routes>
+            <Route path="/join-now" element={<JoinNow />} />
+          </Routes>
+        </MemoryRouter>
+      </HelmetProvider>,
     );
 
     expect(screen.getByText(/facebook ad/i)).toBeInTheDocument();
